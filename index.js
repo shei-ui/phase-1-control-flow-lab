6a4665e29d927be9bc18f05e51ac1f56c63c3af0
christopherchege <chrisshege35@gmail.com>
2025-03-12 14:54:01 +0300

    changes injavascript file

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+  if(ride <=400){
+  return'This one is on me!'
+  }
+  else if(ride>400 && ride <=2000){
+return'That will be twenty bucks.'
+  }
+  else if(ride>2000 && ride<=2500){
+   return 'I will gladly take your thirty bucks.'
+  }
+  else(ride>2500);{
+   return'No can do.'
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  return city==='NYC'?'Ok, sounds good.':'No go.';
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case'generous':
+    return'Thank you so much.';
+    case'not as generous':
+    return'Thank you.';
+    default:
+      return'Bye.';
+  }
   // Write your code here!
 }
